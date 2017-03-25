c728bfd2ef37beb5f093d4a9f90c261b8760d33c
tsfehshan <ehtousif@gmail.com>
2017-03-26 00:49:53 +0600

    List sorted accordingly

---
+++ src/ListSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ListSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> str = new List<string> { "1", "2", "5", "10", "3", "4", "BBB", "AAA", "1A" , "2BV" };
+            Func<string, int> intParser = input =>
+            {
+                int result;
+                if (!int.TryParse(input, out result))
+                    return int.MaxValue;
+
+                return result;
+            };
+
+            var query = str.OrderBy(s=>s).OrderBy(c => intParser(c));
+        }
+    }
+}
